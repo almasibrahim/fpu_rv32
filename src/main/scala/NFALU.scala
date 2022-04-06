a9369edc1df09705f3cadf7a7c74ce3b3442501a
almasibrahim <almas.ibrahim112@gmail.com>
2022-04-06 05:16:26 +0500

    modified

--- src/main/scala/NFALU.scala
+++ src/main/scala/NFALU.scala
@@ -29,19 +29,107 @@ class NFALU extends Module {
 
     val resFrac = Wire(UInt(23.W))
     val resExp = Wire(UInt(8.W))
+
+
+    val temFrac = Wire(UInt(24.W))
+    val temExp = Wire(UInt(8.W))
+    
     resFrac := 0.U
+    resExp := 0.U
+    temFrac := 0.U
+    temExp := 0.U
+    
 
-    resExp := expBitIn1 + 1.U
-
+   
     when(xorSign && (fracBitIn1 === fracBitIn2)){
         io.result := 0.U
+    }.elsewhen(xorSign && (fracBitIn1 > fracBitIn2)){
+        resFrac := fracBitIn1 - fracBitIn2
+        resExp := expBitIn1
+        io.result := Cat(signBitIn1, temExp, temFrac(22,0))
+    }.elsewhen(xorSign && (fracBitIn1 < fracBitIn2)){
+        resFrac := fracBitIn2 - fracBitIn1
+        resExp := expBitIn1
+        io.result := Cat(signBitIn2, temExp, temFrac(22,0))
     }.otherwise{
         when(fracBitIn1 === fracBitIn2){
             resFrac := fracBitIn1
         }.otherwise{
             resFrac := (fracBitIn1 + fracBitIn2) >> 1.U
         }
+        resExp := expBitIn1 + 1.U
         io.result := Cat(andSign, resExp, resFrac)
     }
+   
+
+
+   when(resFrac(22)=/=1.U && resExp=/=0.U){
+        when (resFrac(22,3)==="b00000000000000000001".U){
+            temFrac := resFrac <<20.U
+            temExp := resExp - 20.U
+        }.elsewhen(resFrac(22,4)==="b0000000000000000001".U){
+            temFrac := resFrac <<19.U
+            temExp := resExp - 19.U
+        }.elsewhen(resFrac(22,5)==="b000000000000000001".U){
+            temFrac := resFrac <<18.U
+            temExp := resExp - 18.U
+        }.elsewhen(resFrac(22,6)==="b00000000000000001".U){
+            temFrac := resFrac <<17.U
+            temExp := resExp - 17.U
+        }.elsewhen(resFrac(22,7)==="b0000000000000001".U){
+            temFrac := resFrac <<16.U
+            temExp := resExp - 16.U
+        }.elsewhen(resFrac(22,8)==="b000000000000001".U){
+            temFrac := resFrac <<15.U
+            temExp := resExp - 15.U
+        }.elsewhen(resFrac(22,9)==="b00000000000001".U){
+            temFrac := resFrac <<14.U
+            temExp := resExp - 14.U
+        }.elsewhen(resFrac(22,10)==="b0000000000001".U){
+            temFrac := resFrac <<13.U
+            temExp := resExp - 13.U
+        }.elsewhen(resFrac(22,11)==="b000000000001".U){
+            temFrac := resFrac <<12.U
+            temExp := resExp - 12.U
+        }.elsewhen(resFrac(22,12)==="b00000000001".U){
+            temFrac := resFrac <<11.U
+            temExp := resExp - 11.U
+        }.elsewhen(resFrac(22,13)==="b0000000001".U){
+            temFrac := resFrac <<10.U
+            temExp := resExp - 10.U
+        }.elsewhen(resFrac(22,14)==="b000000001".U){
+            temFrac := resFrac <<9.U
+            temExp := resExp - 9.U
+        }.elsewhen(resFrac(22,15)==="b00000001".U){
+            temFrac := resFrac <<8.U
+            temExp := resExp - 8.U
+        }.elsewhen(resFrac(22,16)==="b0000001".U){
+            temFrac := resFrac <<7.U
+            temExp := resExp - 7.U
+        }.elsewhen(resFrac(22,17)==="b000001".U){
+            temFrac := resFrac <<6.U
+            temExp := resExp - 6.U
+        }.elsewhen(resFrac(22,18)==="b00001".U){
+            temFrac := resFrac <<5.U
+            temExp := resExp - 5.U
+        }.elsewhen(resFrac(22,19)==="b0001".U){
+            temFrac := resFrac <<4.U
+            temExp := resExp - 4.U
+        }.elsewhen(resFrac(22,20)==="b001".U){
+            temFrac := resFrac <<3.U
+            temExp := resExp - 3.U
+        }.elsewhen(resFrac(22,21)==="b01".U){
+            temFrac := resFrac <<2.U
+            temExp := resExp - 2.U
+        }.otherwise{
+            temFrac := 0.U
+            temExp := 0.U
+        }
+    }.otherwise{
+        temFrac:=resFrac
+        temExp:= resExp
+    }
+ 
 
 }
+
